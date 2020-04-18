c25fa66fbebe3da89bf93598b4522605e269eaeb
Dmytro Nasyrov <dmytro@pharosproduction.com>
2020-04-18 14:07:10 +0300

    create backbone

---
+++ src/test/java/supplychain/brewery/BreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package supplychain.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
